ef3efbb15d6babf1b74147538d67260f85bd67e4
tuuli <tuuli.mrl@gmail.com>
2026-02-02 19:09:21 +0200

    Add improved integer input function with extra character validation, that is not fully functionat yet. #ifdeffillä pois käytöstä, tiedosto toimii normaalisti.

--- My_C_header.c
+++ My_C_header.c
@@ -30,6 +30,33 @@ int read_range(int low, int high){
     return user_input;
 }
 
+// Improved version with extra character check, does not allow extra characters after integer input, does not work yet, fucking pain in the ass,
+//Luulin et sain sen ratkastua viimevuonna, mutta ei näköjään toimikkaan..
+////LÖYTYY MYÖS C_rogramming_K26 projektin Week2_T4.c tiedostosta, VARO RISTIMUOKKAUSTA!!
+#if 0
+int read_integer2(void){
+    int num = 0;
+    char wrongchr;
+    //while ( (scanf(" %d %c", &num, &wrongchr) != 2 ) ){     //(wrongchr != '\n')
+    while ( (scanf(" %d", &num) != 1 || scanf(" %c", &wrongchr) == 1 ) ){
+        //if(scanf("%d", &num) != 1) {    //scanf("%c", &extra) == EOF) //Miks tää ei toimi ylimääräsen virhesyötteen eliminoinnissa?
+
+        /*
+        if (wrongchr != '\n') {
+            while(getchar() != '\n');
+        }*/
+        while(getchar() != '\n');
+        printf("Invalid input, try again\n");
+
+
+        //}
+    }
+    while(getchar() != '\n');
+    printf("Successful entering \n");
+    return num;
+}
+#endif
+
 //DICE ROLLING FUNCTIONS
 
 int roll_dice_X(int dice_X){
